10c0ee5a4ce833f47f676296e62423d315c49b52
shengli <363912809@qq.com>
2019-01-05 15:01:57 +0800

    weixin

--- application/api/controller/WeixinPublic.php
+++ application/api/controller/WeixinPublic.php
@@ -15,8 +15,33 @@ class WeixinPublic extends Base {
 		parent::__construct();
 	}
 
-	public function index(){
-		
+	// 微信公众平台接口配置验证
+	public function configAuth(){
+// 		$params = array (
+//   'signature' => '9d32c6da00aea54cedc9a8d14e2c73d0ce159233',
+//   'echostr' => '1442968030760087503',
+//   'timestamp' => '1546658631',
+//   'nonce' => '1235037838',
+// );
+
+		$params = input('get.');
+		if(empty($params)) return false;
+
+
+		$signature  = $params['signature'];
+		$timestamp   = $params['timestamp'];
+		$nonce  = $params['nonce'];
+		$echostr  = $params['echostr'];
+		$token  = 'jiazheng';
+		$list = array($nonce,$timestamp,$token);
+		sort($list, SORT_STRING);
+		$list = implode(',', $list);
+
+		if(sha1($list) == $signature){
+			echo $echostr;
+		} else {
+			return false;
+		}
 	}
 
 }
