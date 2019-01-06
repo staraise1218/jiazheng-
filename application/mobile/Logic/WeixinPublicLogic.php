c050e90943979cd37b150928c7a7ede564d4279c
shengli <363912809@qq.com>
2019-01-06 21:55:26 +0800

    weixin

--- application/mobile/Logic/WeixinPublicLogic.php
+++ application/mobile/Logic/WeixinPublicLogic.php
@@ -16,7 +16,7 @@ class WeixinPublicLogic {
 		$authUrl = 'https://open.weixin.qq.com/connect/oauth2/authorize';
     	$params = array(
     		'appid' => $this->appid,
-    		'redirect_uri' => urlencode('http://jiazheng.staraise.com.cn/index.php/mobile/weixin/get_access_token'),
+    		'redirect_uri' => urlencode('http://jiazheng.staraise.com.cn/index.php/mobile/weixin/get_userinfo'),
 			'response_type' => 'code',
 			'scope' => 'snsapi_userinfo',
 			'state' => '1234',
@@ -33,7 +33,14 @@ class WeixinPublicLogic {
 	public function get_access_token($code){
 		$url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.$this->appid.'&secret='.$this->appsecret.'&code='.$code.'&grant_type=authorization_code';
 
-		$access_token = file_get_contents($url);
-		p($access_token);
+		$result = file_get_contents($url);
+		return json_decode($result);
+	}
+
+	public function get_userinfo($access_token_info){
+		$url = 'https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token_info['access_token'].'&openid='.$access_token_info['openid'].'&lang=zh_CN';
+
+		$result = file_get_contents($url);
+		return $json_decode($result);
 	}
 }
--- application/mobile/controller/Weixin.php
+++ application/mobile/controller/Weixin.php
@@ -8,6 +8,7 @@ use app\mobile\Logic\WeixinPublicLogic;
 
 class Weixin {
 
+	// 第一步获取 code
     public function get_code(){
     	// getcode
     	$WeixinPublicLogic = new WeixinPublicLogic();
@@ -16,12 +17,15 @@ class Weixin {
     	header("Location:$authUrl");
     	die();
     }
-
-    public function get_access_token(){
+    // 获取code 后的回调，获取用户信息，1先获取access_token, 2 获取用户信息
+    public function get_userinfo(){
     	$code = input('code', 0);
+    	// 通过code 获取access_token
     	$WeixinPublicLogic = new WeixinPublicLogic();
-        $access_token = $WeixinPublicLogic->get_access_token($code);
+        $access_token_info = $WeixinPublicLogic->get_access_token($code);
+
+        // 获取用户信息
+        $userinfo = $WeixinPublicLogic->get_userinfo($access_token_info);
+        p($userinfo);
     }
-
-
 }
