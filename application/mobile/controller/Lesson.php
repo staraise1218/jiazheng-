f5364da951c648369242a44d5ec85e1999f00d9d
shengli <363912809@qq.com>
2019-01-06 23:13:04 +0800

    lesson

--- application/mobile/controller/Lesson.php
+++ application/mobile/controller/Lesson.php
@@ -20,4 +20,57 @@ class Lesson extends Base{
 		$this->assign('info', $info);
     	return $this->fetch();
     }
+
+    public function submitOrder(){
+        $lesson_id = I('lesson_id');
+        $user_id = I('user_id');
+
+        // 检测数据是否存在
+        $lesson = Db::name('lesson')
+            ->where('id', $lesson_id)
+            ->find();
+        if(empty($lesson)) response_error('', '数据不存在');
+
+        $order_sn = $this->generateOrderSn();
+        $data = array(
+            'lesson_id' => $lesson_id,
+            'price' => $video['price'],
+            'user_id' => $user_id,
+            'order_sn' => $order_sn,
+            'createtime' => time(),
+        );
+
+        if(Db::name('lesson_order')->insert($data)){
+            $resultData = array(
+                'order_sn' => $order_sn,
+            );
+            response_success($resultData, '下单成功');
+        } else {
+            response_error('', '下单失败');
+        }
+    }
+
+    // 记录播放课程、集数、时间
+    public function ajaxPlayedLog(){
+    	$data['user_id'] = input('user_id');
+    	$data['lesson_id'] = input('lesson_id');
+    	$data['lesson_episode_id'] = input('lesson_episode_id');
+    	$data['current_time'] = input('current_time');
+    	$data['ended'] = input('ended');
+
+    	if(Db::name('lesson_played')->insert($data)){
+    		response_success('', '操作成功');
+    	} else {
+    		response_error('', '操作失败');
+    	}
+    }
+
+   private function generateOrderSn(){
+        $order_sn = date('YmdHis').mt_rand(1000, 9999);
+
+        $count = Db::name('video_order')->where('order_sn', $order_sn)->count();
+        if($count) $this->generateOrderSn();
+
+        return $order_sn;
+    }
 }
