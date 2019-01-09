b151597e5cc2f082dc67eb4b5ef5ca538673f0f4
guofeng-mi <guofeng_mi@sina.com>
2019-01-10 00:50:05 +0800

    011001

--- application/mobile/view/static/js/video.js
+++ application/mobile/view/static/js/video.js
@@ -5,12 +5,12 @@ var video = $(".video")[0], // video 组件
     continue_wrap = $(".hint-wrap")[0], // 继续播放wrap
     continue_btn = $(".continue")[0], // 继续播放按钮
     video_btn = $(".video-btn"),
-    video_btn_wrap = $(".wrap")[0]
+    video_btn_wrap = $(".wrap")[0],
+    number_len = $('.wrap li .video-btn').length;
 
-    
-var number_len = $('.wrap li .video-btn').length;
-
-
+var is_play = false,
+    can_play = true,
+    item_btn_change = false;
 
 var lastplay_title = $(".lastplay_title").get(0).value,
     lastplay_number = $(".lastplay_number").get(0).value == "" ? $(".wrap .active a:eq(0)").attr("number") : $(".lastplay_number").get(0).value; // 当前播放集数
@@ -31,17 +31,15 @@ var postData = {
     ended: ended
 }
 
-
-
 // 初始化函数
 function init() {
     continueBtnIsnone();
     if(current_time != 0) {
-        console.log(current_time + " 初始化播放时间成功");
+        console.log("current_time :" +current_time + " --> 初始化播放时间成功");
         current(current_time);
     } else {
         current(0)
-        console.log("current_time")
+        console.log("current_time : " + current_time + " --> init()")
     }
 }
 
@@ -49,18 +47,24 @@ function init() {
 // 视频加载完成  oncanplay
 video.onloadeddata = function(){
     init();
-    console.log("视频--加载完成")
+    console.log("current_time :"+ current_time + ": --> 视频--加载完成")
 }
 
 // 分集，事件代理函数
 $(video_btn_wrap).delegate("a","click",function(){
-    number = $(this).attr("number")
+    number = $(this).attr("number");
     if(number == lastplay_number) {
-        video.src = $(this).attr("data-video");
+        if(!is_play) {
+            if(item_btn_change) {
+                console.log("number : " + number + " --> 第...集")
+                video.currentTime = 0;
+                item_btn_change = false;
+                can_play = true;
+                video.src = $(this).attr("data-video");
+            }
+        }
     }   
 });
-
-
 
 // 设置从……开始播放
 function current(time) {
@@ -68,12 +72,10 @@ function current(time) {
 }
 // 开始播放  点击video中按钮
 play_btn_img.onclick = function (e) {
-    // current_time =current_time;
     video_play(e);
 }
 // 开始播放  点击继续播放按钮
 continue_btn.onclick = function (e) {
-    // current(current_time);
     video_play(e);
 }
 // 判断继续播放部分是否显示
@@ -86,31 +88,35 @@ function continueBtnIsnone() {
 }
 // 视频播放
 function video_play (e) {
-    video.play();
-    play_none()
-    e.stopPropagation();
-    console.log("视频开始播放");
+    if(can_play) {
+        is_play = true;
+        video.play();
+        play_none();
+        e.stopPropagation();
+        console.log("current_time : " + current_time + " --> 视频开始播放");
+    }
 }
 
 // 暂停 记录时间
 video.onclick = function () {
+    is_play = false;
     video.pause();
     play_block();
     current_time = Math.floor(video.currentTime);
     postData.current_time = current_time;
     console.log('播放暂停 current_time :' + current_time);  // 记录current_time
     console.log(postData)
-    $.ajax({
-        type: 'POST',
-        data: postData,
-        url: 'http://jiazheng.staraise.com.cn/mobile/lesson/ajaxPlayedLog',
-        success: function () {
-            console.log(postData)
-        },
-        error: function (e) {
-            console.log("error -- 暂停");
-        }
-    })
+    // $.ajax({
+    //     type: 'POST',
+    //     data: postData,
+    //     url: 'http://jiazheng.staraise.com.cn/mobile/lesson/ajaxPlayedLog',
+    //     success: function () {
+    //         console.log(postData)
+    //     },
+    //     error: function (e) {
+    //         console.log("error -- 暂停");
+    //     }
+    // })
 }
 
 function play_none() {
@@ -123,28 +129,33 @@ function play_none() {
 function play_block() {
     play_btn.style.display = "flex";
     play_btn_img.style.display = "inline-block";
-
 }
 
 // 视频播放完成 记录时间
 video.onended = function() {
-    console.log("视频播放完成"); // 记录播放完成
+    is_play = false;
+    can_play = false;
+    item_btn_change = true;
+
+    console.log("current_time :" + current_time + ": --> 视频播放完成"); // 记录播放完成
     postData.ended = 1;
+    postData.current_time = 0;
     number += 1;
     lastplay_number = postData.number += 1;
+
     play_block();
     console.log(postData);
-    $.ajax({
-        type: 'POST',
-        data: postData,
-        url: 'http://jiazheng.staraise.com.cn/mobile/lesson/ajaxPlayedLog',
-        success: function () {
-            console.log(postData)
-        },
-        error: function (e) {
-            console.log("error -- 播放完成");
-        }
-    })
+    // $.ajax({
+    //     type: 'POST',
+    //     data: postData,
+    //     url: 'http://jiazheng.staraise.com.cn/mobile/lesson/ajaxPlayedLog',
+    //     success: function () {
+    //         console.log(postData)
+    //     },
+    //     error: function (e) {
+    //         console.log("error -- 播放完成");
+    //     }
+    // })
 };
 
 // 监听页面关闭
@@ -155,17 +166,17 @@ window.onbeforeunload=function(e){
     current_time = Math.floor(video.currentTime);
     postData.current_time = current_time;
     console.log(postData);
-    $.ajax({
-        type: 'POST',
-        data: postData,
-        url: 'http://jiazheng.staraise.com.cn/mobile/lesson/ajaxPlayedLog',
-        success: function () {
-            console.log(postData)
-        },
-        error: function (e) {
-            console.log("error -- 关闭页面");
-        }
-    })
+    // $.ajax({
+    //     type: 'POST',
+    //     data: postData,
+    //     url: 'http://jiazheng.staraise.com.cn/mobile/lesson/ajaxPlayedLog',
+    //     success: function () {
+    //         console.log(postData)
+    //     },
+    //     error: function (e) {
+    //         console.log("error -- 关闭页面");
+    //     }
+    // })
 }
 
 
@@ -183,7 +194,7 @@ var nowIndex = 0,
     w = $('.chapter-part').width(),
     len = $('.item').length,
     slider_timer = undefined,
-    flag = true;
+    flag_poster = true;
 bindEvent();
 function bindEvent() {
     $('.prevBtn').add($('.nextBtn')).add($('.item')).on('click', function () {
@@ -211,8 +222,8 @@ function bindEvent() {
     })
 }
 function move(direction) {
-    if(flag) {
-        flag = false;
+    if(flag_poster) {
+        flag_poster = false;
         var a = 1;
         if(direction == 'prev' || direction == 'next') {
             if(direction == 'prev'){
@@ -228,7 +239,7 @@ function move(direction) {
                     $('.wrap').animate({left: -(w * len)}, function () {
                         $(this).css({left: 0});
                         clearTimeout(slider_timer);
-                        flag = true;
+                        flag_poster = true;
                     })
                     nowIndex = 0;
                 }else {
@@ -241,7 +252,7 @@ function move(direction) {
         if(a) {
             $('.wrap').animate({left: -(w * nowIndex)}, function () {
                 clearTimeout(slider_timer);
-                flag = true;
+                flag_poster = true;
             });
         }
     }
