ec9c7c6fac7511fa676f7df66d57378cbd14a3b5
guofeng-mi <guofeng_mi@sina.com>
2019-01-09 18:20:32 +0800

    gf010920

---
+++ application/mobile/view/static/js/subscribe.js
@@ -0,0 +1,84 @@
+var oUl = $(".wrapper ul").get(0)
+
+
+
+
+var dataPost = {
+    page: 1,
+    cat_id: 1
+}
+
+
+
+
+
+
+
+
+
+
+
+
+$(".nav-wrap").delegate("li","click", function(e) {
+    console.log($(this).index())
+})
+
+$.ajax({
+    type: "POST",
+    url: "http://jiazheng.staraise.com.cn/index.php/api/aunt/getlist",
+    data: dataPost,
+    success: function (data) {
+        createDom(data.data)
+    },
+    error: function () {
+        console.log("error")
+    }
+})
+
+
+
+function createDom (data) {
+    var str = '';
+    console.log(data)
+    for(var i = 0; i < data.length; i++) {
+        str += 
+        '<li>\
+            <div class="poster">\
+                <img src="'+data[i].thumb+'" alt="poster">\
+            </div>\
+            <div class="right">\
+                <div class="title">\
+                    <strong style="margin-right:0.2rem">'+ data.title +'</strong>\
+                    <span>'+data[i].leixing+'</span>\
+                </div>\
+                <div class="price price-symbol" style="color:#8B8B8B">\
+                </div>\
+                <div class="skill">\
+                    '+data[i].tag+'\
+                </div>\
+                <div class="right-pay-wrap">\
+                    <div class="pay">\
+                        <button class="pay-btn">联系家政经纪人</button>\
+                    </div>\
+                </div>\
+            </div>\
+        </li>'
+    }
+    oUl.innerHTML = str;
+    console.log(str)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
