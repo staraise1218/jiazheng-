bc2a5b4289db3767c82ed49678e2e85f7c060312
shengli <363912809@qq.com>
2019-01-09 16:17:42 +0800

    aunt

--- application/mobile/controller/Aunt.php
+++ application/mobile/controller/Aunt.php
@@ -8,7 +8,30 @@ use think\Db;
 class Aunt extends Base{
 
     public function index(){
-    	
+    	// 获取banner
+		$bannerList = Db::name('ad')
+			->where('pid', 2)
+			->where('enabled', 1)
+			->field('ad_name, ad_link, ad_code')
+			->order('orderby desc, ad_id desc')
+			->select();
+
+		// 获取阿姨分类
+		$auntCategoryList = Db::name('aunt_category')->select();
+		// 获取阿姨列表
+		$auntList = Db::name('aunt')
+			->where('is_delete', 0)
+			->limit(10)
+			->select();
+		if(is_array($auntList) && !empty($auntList)){
+			foreach ($auntList as &$item) {
+				$item['tag'] = $item['tag'] ? explode(',', rtrim($item['tag'], ',')) : '';
+			}
+		}
+
+		$this->assign('bannerList', $bannerList);
+		$this->assign('auntCategoryList', $auntCategoryList);
+		$this->assign('auntList', $auntList);
     	return $this->fetch();
     }
 
