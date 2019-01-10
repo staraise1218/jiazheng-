9bd47bc6c36f9b30f26f22af81e6516cef8f0b11
shengli <363912809@qq.com>
2019-01-10 19:16:59 +0800

    lesson

--- application/mobile/view/static/js/mylesson.js
+++ application/mobile/view/static/js/mylesson.js
@@ -36,7 +36,7 @@ function AjaxFunc() {
 function createDom(data) {
     var str = '';
     $.each(data, function(){
-        str += '<li>\
+        str += '<li onclick="window.location=/index.php/mobile/lesson/detail/id/'+this.id+'">\
                     <div class="poster">\
                         <img src="'+this.thumb+'" alt="poster">\
                     </div>\
