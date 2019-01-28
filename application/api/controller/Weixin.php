b91068d5c078e51ef89036949fbcb5f368a8c080
shengli <363912809@qq.com>
2019-01-28 20:43:21 +0800

    weixin

--- application/api/controller/Weixin.php
+++ application/api/controller/Weixin.php
@@ -5,14 +5,7 @@ namespace app\api\controller;
 use think\Controller;
 use think\Db;
 
-class Weixin extends Base {
-
-	public function __construct(){
-		// 设置所有方法的默认请求方式
-		$this->method = '*';
-
-		parent::__construct();
-	}
+class Weixin {
 
 	/**
 	 * [configAuth 公众号配置url 验证消息来自微信服务器]
