2596aae0b97cfbe8d527bdddf5ee4ad0e24933e0
shengli <363912809@qq.com>
2019-02-02 14:37:11 +0800

    page

---
+++ application/mobile/controller/Article.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\mobile\controller;
+
+use think\Controller;
+use think\Db;
+
+class Index extends Base{
+
+    public function about(){
+    	$info = Db::name('article')
+    		->where('article_id', 1)
+    		->where('is_open', 1)
+    		->find();
+
+
+		$this->assign('info', $info);
+    	return $this->fetch('index');
+    }
+}
