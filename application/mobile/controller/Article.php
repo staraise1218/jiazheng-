bd0fcfb7943961aa7cde7c32fc23fa21af720c0b
shengli <363912809@qq.com>
2019-02-02 14:39:03 +0800

    page

--- application/mobile/controller/Article.php
+++ application/mobile/controller/Article.php
@@ -12,7 +12,7 @@ class Article extends Base{
     		->where('article_id', 1)
     		->where('is_open', 1)
     		->find();
-
+    	if($info['content']) $info['content'] = htmlspecialchars_decode($info['content']);
 
 		$this->assign('info', $info);
     	return $this->fetch();
