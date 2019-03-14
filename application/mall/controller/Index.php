7247753664610a094fab6e595c7a7186ebc695da
shengli <363912809@qq.com>
2019-03-14 14:19:38 +0800

    banner

--- application/mall/controller/Index.php
+++ application/mall/controller/Index.php
@@ -12,7 +12,7 @@ class Index extends Base{
     	
     	// 获取banner
 		$bannerList = Db::name('ad')
-			->where('pid', 1)
+			->where('pid', 3)
 			->where('enabled', 1)
 			->field('ad_name, ad_link, ad_code')
 			->order('orderby desc, ad_id desc')
